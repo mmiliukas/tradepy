ebfcf196a5e6eef2f2a2c7d430da01be4c415beb
Mantas Miliukas <mmiliukas@gmail.com>
2025-03-04 21:18:56 +0200

    .

--- tradepy/functions/growth.py
+++ tradepy/functions/growth.py
@@ -4,3 +4,7 @@ import pandas as pd
 def growth(df: pd.DataFrame) -> None:
     df["Growth High"] = (df["High"] / df["Open"]) - 1
     df["Growth Low"] = (df["Low"] / df["Open"]) - 1
+    df["Growth High 1"] = (df["High"].shift(-1) / df["Open"]) - 1
+    df["Growth High 2"] = (df["High"].shift(-2) / df["Open"]) - 1
+    df["Growth Low 1"] = (df["Low"].shift(-1) / df["Open"]) - 1
+    df["Growth Low 2"] = (df["Low"].shift(-2) / df["Open"]) - 1
