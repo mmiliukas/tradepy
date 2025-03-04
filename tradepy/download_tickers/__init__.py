85de9fd030dfd112a0e7b003a0291630f3930107
Mantas Miliukas <mmiliukas@gmail.com>
2025-03-04 20:50:44 +0200

    .

---
+++ tradepy/download_tickers/__init__.py
@@ -0,0 +1,1 @@
+from .download_tickers import download_tickers
---
+++ tradepy/download_tickers/download_tickers.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def download_tickers(
+    source: str = "https://raw.githubusercontent.com/mmiliukas/screeners/main/tickers.csv",
+    exclude_exchange: list[str] = ["PNK"],
+    include_screeners: list[str] = ["Winners 10"],
+):
+    df = pd.read_csv(source)
+
+    if "Exchange" in df.columns:
+        if exclude_exchange and len(exclude_exchange):
+            df = df[~df["Exchange"].isin(exclude_exchange)]
+
+    if "Screener Max" in df.columns:
+        if include_screeners and len(include_screeners):
+            df = df[df["Screener Max"].isin(include_screeners)]
+
+    return list(zip(df["Symbol"].to_list(), df["Screener Max"].to_list()))
