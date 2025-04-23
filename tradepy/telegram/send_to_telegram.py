59ead6d175a625cbe879b0fb3129e7a55f72f97e
Mantas Miliukas <mmiliukas@gmail.com>
2025-04-23 21:32:01 +0300

    .

--- tradepy/telegram/send_to_telegram.py
+++ tradepy/telegram/send_to_telegram.py
@@ -6,6 +6,21 @@ TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 
 
+def send_to_telegram_text(
+    text: str,
+    chat_id=TELEGRAM_CHAT_ID,
+    token=TELEGRAM_TOKEN,
+) -> None:
+    url = f"https://api.telegram.org/bot{token}/sendMessage"
+    params = {
+        "chat_id": chat_id,
+        "text": text,
+    }
+
+    response = requests.post(url, params=params)
+    response.raise_for_status()
+
+
 def send_to_telegram(
     text: str,
     chat_id=TELEGRAM_CHAT_ID,
